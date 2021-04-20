9aded8760f60de637f00b1ad610b17a776240433
EC2 Default User <ec2-user@ip-172-31-41-100.ec2.internal>
2021-04-20 17:47:41 +0000

    after exercise 6

--- directory.rb
+++ directory.rb
@@ -42,7 +42,7 @@ def print_header
 end
 def print(students)
   students.each_with_index() do |student,index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) and the mischief is #{student[:crime]}"
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) and the mischief is #{student[:crime]}".center (80)
   end
 end
 
