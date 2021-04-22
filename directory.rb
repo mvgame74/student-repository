fee9691ad16c30a5b743be778b250156a75d8bf1
EC2 Default User <ec2-user@ip-172-31-41-100.ec2.internal>
2021-04-22 10:14:12 +0000

    after exercise 9

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-#let´s put all the students into an array
-=begin
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end
 
 def input_students
   puts "Please enter the names of the students"
@@ -32,37 +16,38 @@ def input_students
     my_crime = "unknown" if my_crime == ""
     #add the student hash to the array
     students << {name:name, cohort:my_cohort, crime:my_crime}
-    puts "Now we have #{students.count} students"
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     #get another name from the user
     name = gets.chomp
   end
   students
 end
 
-
 def print_header
   puts "The students of Villains Academy"
-  puts "-------------"
+  puts "________________________________"
 end
+
 def print(students)
-  #students = students.sort_by {|value| value[:cohort].to_i}
   cohorts = students.map do |student|
       student[:cohort]
     end
     cohorts.uniq.each do |cohort|
-      puts "#{cohort} cohort".upcase.center(80)
+      puts "#{cohort} cohort".capitalize.center(80)
         students.each do |student|
           puts "#{student[:name]} and the mischief is #{student[:crime]}" if student[:cohort] == cohort
         end
     end
-  #students.each_with_index() do |student,index|
-  #  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) and the mischief is #{student[:crime]}".center (80)
-  #end
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
 #nothing happens until we call the methods
 students = input_students
 print_header
