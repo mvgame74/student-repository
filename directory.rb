a5eb8e987f2aa28206ea2be0235bac3c514d7754
EC2 Default User <ec2-user@ip-172-31-41-100.ec2.internal>
2021-04-22 18:02:24 +0000

    after step 10... equal to exercise expectation

--- directory.rb
+++ directory.rb
@@ -1,79 +1,60 @@
-@students = [] #and empty array accesible to all the methods
+@students = [] # an empty array accessible to all methods
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+end
 
 def interactive_menu
   loop do
-   print_menu
-   process(gets.chomp)
+    print_menu
+    process(gets.chomp)
   end
 end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" #because we will be adding more items
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-  end
+  puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def show_students
   print_header
-  print
+  print_student_list
   print_footer
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  name = gets.chomp
-  #while the name is not empty, repeat this code
-  while !name.empty? do
-    puts "what's his/her cohort"
-    my_cohort = gets.chomp
-    my_cohort = "april" if my_cohort == ""
-    puts "what's his/her favorite misdeed?"
-    my_crime = gets.chomp
-    my_crime = "unknown" if my_crime == ""
-    #add the student hash to the array
-    @students << {name:name, cohort:my_cohort, crime:my_crime}
-    if @students.count == 1
-      puts "Now we have 1 student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    #get another name from the user
-    name = gets.chomp
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
 def print_header
   puts "The students of Villains Academy"
-  puts "________________________________"
+  puts "-------------"
 end
 
-def print
-  if @students.count < 1
-    puts "No student enrolled at this school"
-  else
-    cohorts = @students.map do |student|
-      student[:cohort]
-    end
-    cohorts.uniq.each do |cohort|
-      puts "#{cohort} cohort".capitalize.center(80)
-        @students.each do |student|
-          puts "#{student[:name]} and the mischief is #{student[:crime]}" if student[:cohort] == cohort
-        end
-    end
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -81,5 +62,4 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-#nothing happens until we call the methods
 interactive_menu
