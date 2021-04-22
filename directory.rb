b7d25690ff841e7fc3f4c293b9b68a148b0e57c2
EC2 Default User <ec2-user@ip-172-31-41-100.ec2.internal>
2021-04-22 09:54:34 +0000

    after exercise 8 plus clean up old code

--- directory.rb
+++ directory.rb
@@ -24,10 +24,10 @@ def input_students
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
-    puts "what's your cohort"
+    puts "what's his/her cohort"
     my_cohort = gets.chomp
     my_cohort = "april" if my_cohort == ""
-    puts "what's your favorite misdeed?"
+    puts "what's his/her favorite misdeed?"
     my_crime = gets.chomp
     my_crime = "unknown" if my_crime == ""
     #add the student hash to the array
@@ -45,9 +45,19 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  students.each_with_index() do |student,index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) and the mischief is #{student[:crime]}".center (80)
-  end
+  #students = students.sort_by {|value| value[:cohort].to_i}
+  cohorts = students.map do |student|
+      student[:cohort]
+    end
+    cohorts.uniq.each do |cohort|
+      puts "#{cohort} cohort".upcase.center(80)
+        students.each do |student|
+          puts "#{student[:name]} and the mischief is #{student[:crime]}" if student[:cohort] == cohort
+        end
+    end
+  #students.each_with_index() do |student,index|
+  #  puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort) and the mischief is #{student[:crime]}".center (80)
+  #end
 end
 
 def print_footer(students)
