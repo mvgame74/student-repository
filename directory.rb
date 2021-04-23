4eb895a42b8178074c0c7772eec1d8c56256c85a
EC2 Default User <ec2-user@ip-172-31-41-100.ec2.internal>
2021-04-23 06:37:13 +0000

    after step 12, loading students

--- directory.rb
+++ directory.rb
@@ -26,6 +26,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -43,6 +44,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -63,6 +66,7 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+  puts ""
 end
 
 def save_students
@@ -75,6 +79,18 @@ def save_students
     file.puts csv_line
   end
   file.close
+  puts "All the students are saved"
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line| 
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts "All the students are loaded"
+end
+    
 
 interactive_menu
